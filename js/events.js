96a3d7e92e31ca1a73adee2bb77561f65a21b4f2
joshlacey <williamjoshualacey@gmail.com>
2017-09-05 22:34:50 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,40 @@
 //define functions here
+function getIt() {
+ $('p').on('click', function() {
+    alert('Hey!')
+})
+}
+function hello() {
+  console.log(hello)
+}
 
-$(document).ready(function(){
+function frameIt() {
+ $('img').on('load', () => {
+    $('img').addClass('tasty')
+  })
+}
+
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+   if (e.which === 71) {
+     alert("You pressed 'g'!")
+   }
+ })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+$(document).ready(function(x){
+   getIt()
+   hello()
+   frameIt()
+   pressIt()
+   submitIt()
 
 // call functions here
 
